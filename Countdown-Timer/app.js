dd7105ef2c72327581b6c4658e36b8c3d7688d08
Ivan <ivan.halkov@gmail.com>
2023-01-08 22:42:20 +0200

    fixed countdown timer

--- Countdown-Timer/app.js
+++ Countdown-Timer/app.js
@@ -1,7 +1,19 @@
 window.onload = () => {
-    const countdownTimer = document.querySelector('.countdown-timer');
-    addCountdownTimer(countdownTimer);
-    // countdownTimer.textContent = '00:00:00';
+    const el = getDomElem();
+
+    const pressedNumbers = {
+        count: 0,
+        data: [0, 0, 0, 0, 0, 0],
+    };
+
+    const timerValues = {
+        startTime: 0,
+        inputTime: 0,
+        targetTime: 0,
+        timeLeft: 0,
+        intervalId: null,
+        audio: null
+    };
 
     const numpad = document.querySelector('.numpad');
     addNumpadButtons(numpad);
@@ -29,400 +41,253 @@ window.onload = () => {
 
     const continueBtn = document.querySelector('#continueBtn');
     continueBtn.addEventListener('click', onContinue);
-}
 
-function addCountdownTimer(countdownTimer) {
-    const mainTimer = document.createElement('div');
-    mainTimer.classList.add('main-timer');
-    
 
-    const msTimer = document.createElement('div');
-    msTimer.classList.add('ms-timer');
-    msTimer.textContent = '000';
-
-    // set main timer
-    const timerData = ['hours', ':', 'minutes', ':', 'seconds'];
-    for (let i = 0; i < timerData.length; i++) {
-        let currDiv = document.createElement('div');
-
-        if (i % 2 == 0) {
-            currDiv.classList.add(timerData[i]);
-            currDiv.textContent = '00';
-        } else {
-            currDiv.classList.add('colon');
-            currDiv.textContent = timerData[i];
+    function getDomElem() {
+        return {
+            timer: document.querySelector('.timer'),
+            hours: document.querySelector('.hours'),
+            minutes: document.querySelector('.minutes'),
+            seconds: document.querySelector('.seconds'),
         }
-        mainTimer.appendChild(currDiv);
-    }
-    mainTimer.appendChild(msTimer);
-
-    countdownTimer.appendChild(mainTimer);
-    // countdownTimer.appendChild(msTimer);
-}
-
-function addNumpadButtons(numpad) {
-    for (let i = 0; i < 10; i++) {
-        let btn = document.createElement('div');
-        btn.innerText = i;
-        btn.classList.add('numpad-button');
-        btn.id = i;
-        numpad.appendChild(btn);
-    }
-}
-
-function onNumpad(event) {
-    event.preventDefault();
-
-    const clicked = event.target;
-
-    // pressedNumbers.count < 6 because there are 3 two digit numbers
-    // and we don't need changes after that
-    if (clicked.tagName == 'DIV' && clicked.className == 'numpad-button' && pressedNumbers.count < 6) {
-        // move numbers to the left
-        pressedNumbers.data.push(Number(clicked.textContent));
-        pressedNumbers.data.shift();
-        pressedNumbers.count += 1;
-    }
-    updateTimer();
-}
-
-const pressedNumbers = {
-    count: 0,
-    data: [0, 0, 0, 0, 0, 0]
-};
-
-// invoking new object from function
-//  invoking timeElements object as a function to avoid declaring before they are even not created as an elements
-function getTimeElements() {
-    return {
-        hours: document.querySelector('.hours'),
-        minutes: document.querySelector('.minutes'),
-        seconds : document.querySelector('.seconds'),
-        milliseconds: document.querySelector('.ms-timer')
-    };
-}
-
-function updateTimer() {
-    const timeElements = getTimeElements();
-    
-    const arr = pressedNumbers.data;
-    for (let i = 0; i < arr.length - 1; i++) {
-        timeElements.hours.textContent = arr[0] + '' + arr[1];
-        timeElements.minutes.textContent = arr[2] + '' + arr[3];
-        timeElements.seconds.textContent = arr[4] + '' + arr[5];
-    }
-}
-
-function getCurrTimeMs() {
-    const dt = new Date();
-    return dt.getTime();
-}
-
-function getCountdownTime() {
-    const timeElements = getTimeElements();
-
-    return {
-        hours: Number(timeElements.hours.textContent),
-        minutes: Number(timeElements.minutes.textContent),
-        seconds: Number(timeElements.seconds.textContent),
-        milliseconds: Number(timeElements.milliseconds.textContent)
-    }
-}
-
-function updateCountdownTime(h, m, sec, ms) {
-    const timeElements = getTimeElements();
-    
-    timeElements.hours.textContent = formatTimeElem(h);
-    timeElements.minutes.textContent = formatTimeElem(m);
-    timeElements.seconds.textContent = formatTimeElem(sec);
-    timeElements.milliseconds.textContent = formatMs(ms);
-}
-
-function formatMs(ms) {
-    if (ms < 10) {
-        ms = '00' + ms;
-    } else if (ms < 100) {
-        ms = '0' + ms;
     }
 
-    return ms;
-}
-
-function formatTimeElem(el) {
-    if (el < 10) {
-        el = '0' + el;
-    } 
-
-    return el;
-}
-
-function fromTimeToMs(h, min, sec, ms) {
-    let milliseconds = ms;
-    milliseconds += sec * 1000;
-    milliseconds += min * 60 * 1000;
-    milliseconds += h * 60 * 60 * 1000;
-    return milliseconds;
-}
-
-function fromMsToTime(ms) {
-    return {
-        hours: Math.floor(ms / 1000 / 60 / 60),
-        minutes: Math.floor(ms / 1000 / 60) % 60,
-        seconds: Math.floor(ms / 1000) % 60,
-        milliseconds: ms % 1000
-    }
-}
-
-
-const countdown = {
-    intervalId: 0,
-    msInterval: 0,
-    timeLeft: 0,
-    pausedTime: 0,
-    msLeft: 999,
-    pausedMs: 0
-};
-
-function onSet() {
-    const timeVal = Object.values(getCountdownTime());
-    countdown.targetTimeMs = fromTimeToMs(...timeVal);
-
-    if (countdown.targetTimeMs === 0) {
-        countdown.targetTimeMs = 10000;
+    function addNumpadButtons(numpad) {
+        for (let i = 0; i < 10; i++) {
+            let btn = document.createElement('div');
+            btn.innerText = i;
+            btn.classList.add('numpad-button');
+            btn.id = i;
+            numpad.appendChild(btn);
+        }
     }
 
-    countdown.timeLeft = countdown.targetTimeMs;
-    updateCountdownTime(...Object.values(fromMsToTime(countdown.timeLeft)));
-    showHideControls();
-    showStartButton();
-}
+    function onNumpad(event) {
+        event.preventDefault();
 
-function onStart() {
-    startBtn.classList.add('hide');
-    pauseBtn.classList.remove('hide');
-
-    countdown.intervalId = setTimeInterval();
-    countdown.msInterval = setMsInterval();
-}
-
-function onContinue() {
-    countdown.timeLeft = countdown.pausedTime;
-
-    switchPauseContinue();
-
-    countdown.intervalId = setTimeInterval();
-    countdown.msInterval = setMsInterval();
-}
-
-function updateMS(ms) {
-    const timeElements = getTimeElements();
-    timeElements.milliseconds.textContent = formatMs(ms);
-}
-
-function onPause() {
-    countdown.pausedTime = countdown.timeLeft;
-    clearInterval(countdown.intervalId);
-
-    countdown.pausedMs = countdown.msLeft;
-    clearInterval(countdown.msInterval);
-    switchPauseContinue();
-}
-
-function onClear2() {
-    clearInterval(countdown.intervalId);
-    clearInterval(countdown.msInterval);
-
-    countdown.timeLeft = countdown.targetTimeMs;
-    countdown.pausedTime = 0;
-    countdown.msLeft = 999;
-    countdown.pausedMs = 0;
-    
-    // update timer elements
-    updateCountdownTime(...Object.values(fromMsToTime(countdown.timeLeft)));
-    updateMS(0);
-
-    removeBlink('main-timer');
-    
-    // update object holding data info
-
-    showStartButton();
-    stopAlarmSound();
-}
-
-function showStartButton() {
-    domElem('#continueBtn').classList.add('hide');
-    domElem('#pauseBtn').classList.add('hide');
-
-    domElem('#startBtn').classList.remove('hide');
-}
-
-function domElem(selector) {
-    return document.querySelector(selector);
-}
-
-function onBack() {
-    showHideControls();
-    onClear2();
-    onClear();
-}
-
-function onClear() {
-    const timeElements = getTimeElements();
-    pressedNumbers.count = 0;
-    pressedNumbers.data = [0, 0, 0, 0, 0, 0];
-
-    timeElements.hours.textContent = '00';
-    timeElements.minutes.textContent = '00';
-    timeElements.seconds.textContent = '00';
-    timeElements.milliseconds.textContent = '000';
-
-    countdown.timeLeft = 0;
-    countdown.pausedTime = 0;
-    countdown.msLeft = 999;
-    countdown.pausedMs = 0;
-}
-
-function showHideControls() {
-    const controlButtons = document.querySelector('.control-buttons');
-    const hasHideClass = controlButtons.classList.contains('hide');
-
-    const controls = document.querySelector('.controls');
-    const backBtn = document.querySelector('#backBtn');
-
-    if (hasHideClass) {
-        controlButtons.classList.remove('hide');
-        controls.classList.add('hide');
-        backBtn.classList.remove('hide');
-    } else {
-        controlButtons.classList.add('hide');
-        controls.classList.remove('hide');
-        backBtn.classList.add('hide');
+        const clicked = event.target;
+        if (clicked.tagName == 'DIV' && clicked.className == 'numpad-button' && pressedNumbers.count < 6) {
+            // move numbers to the left
+            pressedNumbers.data.push(clicked.textContent);
+            pressedNumbers.data.shift();
+            pressedNumbers.count += 1;
+            setTimer(pressedNumbers.data.join(''));
+        }
     }
-}
 
-function addControlButtons() {
-    const container = document.querySelector('.container');
-    const controlButtons = document.createElement('div');
-    controlButtons.classList.add('control-buttons');
-    controlButtons.classList.add('hide');
+    function setTimer(str) {
+        // can be /.{2}/g probably
+        const arr = str.toString().match(/.{1,2}/g);
+        // assign time
+        updateTimer(arr[0], arr[1], arr[2]);
+    }
 
-    const startBtn = document.createElement('div');
-    startBtn.id = 'startBtn';
-    startBtn.textContent = 'START';
-    startBtn.classList.add('button');
+    // after each iteration
+    function updateTimer(h, min, sec) {
+        el.hours.textContent = h.toString().padStart(2, 0);
+        el.minutes.textContent = min.toString().padStart(2, 0);
+        el.seconds.textContent = sec.toString().padStart(2, 0);
+    }
 
-    const pauseBtn = document.createElement('div');
-    pauseBtn.id = 'pauseBtn';
-    pauseBtn.classList.add('hide');
-    pauseBtn.textContent = 'PAUSE';
-    pauseBtn.classList.add('button');
+    function fromTimeToMs(h, min, sec) {
+        h = Number(h);
+        min = Number(min);
+        sec = Number(sec);
 
-    const continueBtn = document.createElement('div');
-    continueBtn.id = 'continueBtn';
-    continueBtn.classList.add('hide');
-    continueBtn.textContent = 'CONTINUE';
-    continueBtn.classList.add('button');
+        let ms = sec;
+        ms += min * 60;
+        ms += h * 3600;
+        ms *= 1000;
+        return ms;
+    }
 
-    const clearBtn = document.createElement('div');
-    clearBtn.id = 'clearBtn2';
-    clearBtn.textContent = 'CLEAR';
-    clearBtn.classList.add('button');
+    function fromMsToTime(ms) {
+        document.querySelector('.hours').textContent = (Math.floor(ms / 1000 / 60 / 60)).toString().padStart(2, 0);
+        document.querySelector('.minutes').textContent = (Math.floor(ms / 1000 / 60) % 60).toString().padStart(2, 0);
+        document.querySelector('.seconds').textContent = (Math.floor(ms / 1000) % 60).toString().padStart(2, 0);
+        document.querySelector('.milliseconds').textContent = (ms % 1000).toString().padStart(3, 0);
+    }
 
-    const backBtn = document.createElement('div');
-    backBtn.id = 'backBtn';
-    backBtn.textContent = '<< Back';
-    backBtn.classList.add('hide');
 
-    controlButtons.appendChild(startBtn);
-    controlButtons.appendChild(pauseBtn);
-    controlButtons.appendChild(continueBtn);
-    controlButtons.appendChild(clearBtn);
 
-    
-    container.appendChild(controlButtons);
-    container.appendChild(backBtn);
-}
+    function onSet() {
+        let h = el.hours.textContent;
+        let m = el.minutes.textContent;
+        let s = el.seconds.textContent;
 
-function addBlink(className) {
-    document.querySelector(`.${className}`).classList.add('blink-class');
-}
+        let ms = fromTimeToMs(h, m, s);
+        if (ms === 0) {
+            ms = 10000;
+        }
 
-function removeBlink(className) {
-    document.querySelector(`.${className}`).classList.remove('blink-class');
-}
+        timerValues.inputTime = ms;
+        fromMsToTime(ms);
 
-// function addRemoveBlink(className) {
-//     const blinkClass = 'blink-class';
-//     const hasBlink = elem.classList.contains(blinkClass);
+        showHideControls();
+        showStartButton();
+    }
 
-//     if (hasBlink) {
-//         elem.classList.remove(blinkClass);
-//     } else {
-//         elem.classList.add('blink-class');
-//     }
-// }
+    function onStart() {
+        startBtn.classList.add('hide');
+        pauseBtn.classList.remove('hide');
 
-function switchPauseContinue() {
-    const pauseHasHideClass = document.querySelector('#pauseBtn').classList.contains('hide');
+        timerValues.targetTime = Date.now() + timerValues.inputTime;
+        timerValues.intervalId = setTimeInterval();
+    }
 
-    if (pauseHasHideClass) {
-        document.querySelector('#pauseBtn').classList.remove('hide');
+    function onContinue() {
+        timerValues.targetTime = Date.now() + timerValues.timeLeft;
+        timerValues.intervalId = setTimeInterval();
+        switchPauseContinue();
+    }
+
+    function onPause() {
+        clearInterval(timerValues.intervalId);
+        switchPauseContinue();
+    }
+
+    function onClear2() {
+        clearInterval(timerValues.intervalId);
+        fromMsToTime(timerValues.inputTime);
+
+        removeBlink();
+        showStartButton();
+        stopAlarmSound();
+    }
+
+    function showStartButton() {
         document.querySelector('#continueBtn').classList.add('hide');
-    } else {
         document.querySelector('#pauseBtn').classList.add('hide');
-        document.querySelector('#continueBtn').classList.remove('hide');
+
+        document.querySelector('#startBtn').classList.remove('hide');
     }
-}
 
-function startAlarmSound() {
-    countdown.audio = new Audio('Alarm-Clock.mp3'); // not the best idea
-    countdown.audio.load();
-    countdown.audio.play();
-    countdown.audio.loop = true;
-}
-
-function stopAlarmSound() {
-    const hasAudio = countdown.audio;
-
-    if (hasAudio) {
-        countdown.audio.pause();
-    } else {
-        countdown.audio = new Audio('Alarm-Clock.mp3');
+    function onBack() {
+        showHideControls();
+        onClear2();
+        onClear();
     }
-}
 
-function setTimeInterval() {
-    return setInterval(() => {
-        countdown.timeLeft -= 1000;
-
-        // update countdown timer
+    function onClear() {
+        pressedNumbers.count = 0;
+        pressedNumbers.data = [0, 0, 0, 0, 0, 0];
         
-        updateCountdownTime(...Object.values(fromMsToTime(countdown.timeLeft)));
+        fromMsToTime(0);
+    }
 
-        // update ms-timer
-        if (countdown.timeLeft < 1) {
-            domElem('#pauseBtn').classList.add('hide');
-            addBlink('main-timer');
-            startAlarmSound();
-            clearInterval(countdown.intervalId);
+    function showHideControls() {
+        const controlButtons = document.querySelector('.control-buttons');
+        const hasHideClass = controlButtons.classList.contains('hide');
+
+        const controls = document.querySelector('.controls');
+        const backBtn = document.querySelector('#backBtn');
+
+        if (hasHideClass) {
+            controlButtons.classList.remove('hide');
+            controls.classList.add('hide');
+            backBtn.classList.remove('hide');
+        } else {
+            controlButtons.classList.add('hide');
+            controls.classList.remove('hide');
+            backBtn.classList.add('hide');
         }
+    }
 
-    }, 1000);
-}
+    function addControlButtons() {
+        const container = document.querySelector('.container');
+        const controlButtons = document.createElement('div');
+        controlButtons.classList.add('control-buttons');
+        controlButtons.classList.add('hide');
 
-function setMsInterval() {
-    return setInterval(() => {
-        updateMS(countdown.msLeft);
-        countdown.msLeft -= 21;
+        const startBtn = document.createElement('div');
+        startBtn.id = 'startBtn';
+        startBtn.textContent = 'START';
+        startBtn.classList.add('button');
 
-        if (countdown.timeLeft < 1 && countdown.msLeft <= 1) {
-            updateMS(0);
-            clearInterval(countdown.msInterval);
+        const pauseBtn = document.createElement('div');
+        pauseBtn.id = 'pauseBtn';
+        pauseBtn.classList.add('hide');
+        pauseBtn.textContent = 'PAUSE';
+        pauseBtn.classList.add('button');
+
+        const continueBtn = document.createElement('div');
+        continueBtn.id = 'continueBtn';
+        continueBtn.classList.add('hide');
+        continueBtn.textContent = 'CONTINUE';
+        continueBtn.classList.add('button');
+
+        const clearBtn = document.createElement('div');
+        clearBtn.id = 'clearBtn2';
+        clearBtn.textContent = 'CLEAR';
+        clearBtn.classList.add('button');
+
+        const backBtn = document.createElement('div');
+        backBtn.id = 'backBtn';
+        backBtn.textContent = '<< Back';
+        backBtn.classList.add('hide');
+
+        controlButtons.appendChild(startBtn);
+        controlButtons.appendChild(pauseBtn);
+        controlButtons.appendChild(continueBtn);
+        controlButtons.appendChild(clearBtn);
+
+
+        container.appendChild(controlButtons);
+        container.appendChild(backBtn);
+    }
+
+    function addBlink() {
+        document.querySelector('.timer').classList.add('blink-class');
+    }
+
+    function removeBlink() {
+        document.querySelector('.timer').classList.remove('blink-class');
+    }
+
+    function switchPauseContinue() {
+        const pauseHasHideClass = document.querySelector('#pauseBtn').classList.contains('hide');
+
+        if (pauseHasHideClass) {
+            document.querySelector('#pauseBtn').classList.remove('hide');
+            document.querySelector('#continueBtn').classList.add('hide');
+        } else {
+            document.querySelector('#pauseBtn').classList.add('hide');
+            document.querySelector('#continueBtn').classList.remove('hide');
         }
+    }
 
-        if (countdown.msLeft <= 1) {
-            countdown.msLeft = 999;
+    function startAlarmSound() {
+        timerValues.audio = new Audio('Alarm-Clock.mp3'); // not the best idea
+        timerValues.audio.load();
+        timerValues.audio.play();
+        timerValues.audio.loop = true;
+    }
+
+    function stopAlarmSound() {
+        if (timerValues.audio != null) {
+            timerValues.audio.pause();
+        } else {
+            timerValues.audio = new Audio('Alarm-Clock.mp3');
         }
+    }
 
-    }, 20);
+    function setTimeInterval() {
+
+        return setInterval(() => {
+            timerValues.timeLeft = timerValues.targetTime - Date.now();
+            fromMsToTime(timerValues.timeLeft);
+
+
+            if (timerValues.timeLeft <= 0) {
+                document.querySelector('#pauseBtn').classList.add('hide');
+                addBlink();
+                startAlarmSound();
+                clearInterval(timerValues.intervalId);
+                fromMsToTime(0);
+            }
+
+
+        }, 100);
+    }
+
 }
